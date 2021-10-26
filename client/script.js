de30548e1f03c0e56cee0e2510549689a6b2732d
AashishRauniyar <68764632+AashishRauniyar@users.noreply.github.com>
2021-10-26 20:05:16 +0545

    Update script.js
    added an event that creates an alert box when connected to the server

--- client/script.js
+++ client/script.js
@@ -1,6 +1,10 @@
 const socket = io("http://localhost:3000")
 const textarea = document.querySelector('textarea')
 
+socket.on('connect', (client) => {
+    alert(`Connected with id ${client.id}`)
+})
+
 socket.on('received', syncedText => {
     sycnText(syncedText)
 })
