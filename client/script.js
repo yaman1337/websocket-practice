76340e5f3d1db0aa9ba93ca229eaf280bbeafc04
yaman1337 <63846784+yaman1337@users.noreply.github.com>
2021-10-26 20:03:09 +0545

    Update script.js
    removed connect event logger to optimize code

--- client/script.js
+++ client/script.js
@@ -1,10 +1,6 @@
 const socket = io("http://localhost:3000")
 const textarea = document.querySelector('textarea')
 
-socket.on('connect', () => {
-    console.log(socket)
-})
-
 socket.on('received', syncedText => {
     sycnText(syncedText)
 })
