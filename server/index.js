97b5f9e27550f9880b2c53996bb0bf3bd35713ce
Wtshav Sapkota <111194180+blackspydo@users.noreply.github.com>
2022-10-14 20:23:36 +0545

    add allowedOrigins array

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
+//         add your client domain name here
+const allowedOrigins = ['http://localhost:5500','http://127.0.0.1:5000','https://admin.socket.io/']
 const io = require('socket.io')(3000, {
     cors: {
-//         add your client domain name here
-        origin: ['http://localhost:5500','http://127.0.0.1:5000','https://admin.socket.io/']
+        origin: allowedOrigins
     }
 })
 
